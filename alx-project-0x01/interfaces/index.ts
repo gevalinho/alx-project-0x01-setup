8b9218e8043fc6bfca8e3082a447f10adbe9127b
gevalinho <gevalinho1@gmail.com>
2025-09-17 14:55:59 +0100

    4. More Components (Extending Post Page)

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -38,3 +38,15 @@ export interface CommentProps {
   email: string;
   body: string;
 }
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
