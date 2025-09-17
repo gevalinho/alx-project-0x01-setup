9a0f86a816f21c567659cda538489aa1c50cfc1b
gevalinho <gevalinho1@gmail.com>
2025-09-17 13:51:24 +0100

    3. Implement Users Card Components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
-import PostCard from '../../components/common/PostCard'
-import Button from '../../components/common/Button'
-import Footer from '../../components/layout/Footer'
-import Header from '../../components/layout/Header' 
+import React from "react";
+import { UserProps } from "@/interfaces";
 
-function index() {
+const UserCard: React.FC<UserProps> = ({ name, username, email, phone, website, company }) => {
+  const imageUrl = `https://i.pravatar.cc/150?u=${username}`;
+  const xUrl = `https://twitter.com/${username}`;
+  const linkedinUrl = `https://${website}`;
+
   return (
-    <div>index</div>
-  )
-}
+    <li className="bg-white p-4 rounded-lg shadow">
+      <img
+        alt={name}
+        src={imageUrl}
+        className="aspect-3/2 w-full rounded-2xl object-cover outline-1 -outline-offset-1 outline-black/5"
+      />
+      <h3 className="mt-4 text-lg font-semibold text-gray-900">{name}</h3>
+      <p className="text-sm text-gray-600">{company?.name}</p>
+      <p className="text-xs text-gray-500">{email}</p>
+      <p className="text-xs text-gray-500">{phone}</p>
+      <ul className="mt-4 flex gap-x-4">
+        <li>
+          <a href={xUrl} className="text-gray-400 hover:text-gray-500">
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a href={linkedinUrl} className="text-gray-400 hover:text-gray-500">
+            Website
+          </a>
+        </li>
+      </ul>
+    </li>
+  );
+};
 
-export default index
-
-export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
-
-  return {
-    props: {
-      posts
-    }
-  }
-}
+export default UserCard;
